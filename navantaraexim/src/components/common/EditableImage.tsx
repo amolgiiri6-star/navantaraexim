6796ddb429b19c39e1f3f294506665a1cae79e87
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-10 19:55:45 +0530

    Map herbal wipes product image

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -35,6 +35,7 @@ if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
   if (storageKey === 'prod_img_cs-brush-96033020') return '/Cosmetic Brushes.png';
     if (storageKey === 'prod_img_cs-brush-96033010') return '/Artistic Brushes.png';
     if (storageKey === 'prod_img_cs-sheet-1') return '/Drawer sheets.png';
+    if (storageKey === 'prod_img_cs-wipe-1') return '/Herbal Wipes.png';
 return defaultSrc;
 });
   // Load custom image from IndexedDB on mount if available
