de3bb4a4f5da69c0c1e73f0ea6d9f552d9fe3720
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-11 00:59:36 +0530

    Add Ultra Compact Satin-Twill shirting image mapping

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -43,6 +43,7 @@ if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
     if (storageKey === 'prod_img_fab-twill-4th-60s') return '/Executive 4 Threads.png';
     if (storageKey === 'prod_img_fab-twill-3th-80s') return '/Royal 3 Threads.png';
     if (storageKey === 'prod_img_fab-twill-4th-yd-herringbone') return '/Yarn Dyed 4 threads.png';
+    if (storageKey === 'prod_img_fab-twill-4th-satin-100s') return '/Ultra Compact 4 threads.png';
 return defaultSrc;
 });
   // Load custom image from IndexedDB on mount if available
