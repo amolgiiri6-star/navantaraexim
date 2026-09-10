2ed29057ba238baaa5ce3a2ebe1b83e8d26e7b6d
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-11 02:53:19 +0530

    Add Cut Tobacco image mapping

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -66,6 +66,7 @@ if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
     if (storageKey === 'prod_img_tob-24039920-prep') return '/Preparations Containing Chewing Tobacco.png';
     if (storageKey === 'prod_img_tob-24039930-jarda') return '/zarda_compressed.jpeg';
     if (storageKey === 'prod_img_tob-24039940-snuff') return '/Preparations Containing Chewing Tobacco (2).png';
+    if (storageKey === 'prod_img_tob-24039970-cuttobacco') return '/Cut Tobacco.png';
 return defaultSrc;
 });
   // Load custom image from IndexedDB on mount if available
