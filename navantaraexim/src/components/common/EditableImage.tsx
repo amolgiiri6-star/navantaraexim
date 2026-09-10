77b8d0051e0ad16412900aa052143a439effc5e0
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-11 01:59:37 +0530

    Add Indian Palmarosa Oil image mapping

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -56,6 +56,7 @@ if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
     if (storageKey === 'prod_img_aqueous-distillates-33019060') return '/floral_waters_and_botanical_hydrosol.jpg';
     if (storageKey === 'prod_img_oil-rose-33012938') return '/Indian_Rose_Oil.jpg';
     if (storageKey === 'prod_img_oil-lemongrass-33012942') return '/Lemongrass oil.jpg';
+    if (storageKey === 'prod_img_oil-palmarosa-33012933') return '/Palmarosa_Oil.jpg';
 return defaultSrc;
 });
   // Load custom image from IndexedDB on mount if available
