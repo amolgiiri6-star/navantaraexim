4fdaf3788a28006a9dc754551dcbb84430f4b00c
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-10 12:41:50 +0530

    Update EditableImage.tsx

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -30,6 +30,7 @@ export const EditableImage: React.FC<EditableImageProps> = ({
       if (storageKey === 'prod_img_eo-1') return '/Sandalwood oil.png';
 if (storageKey === 'prod_img_eo-2') return '/peppermint oil.png';
 if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
+   if (storageKey === 'prod_img_eo-4') return '/Palma Rosa.png'; 
 return defaultSrc;
 });
   // Load custom image from IndexedDB on mount if available
