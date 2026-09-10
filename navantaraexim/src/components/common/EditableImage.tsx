cb16b7406a1fab0034cadce1808aaa2628d29e68
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-10 21:53:24 +0530

    Fix translucent setting powder image path

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -39,7 +39,7 @@ if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
     if (storageKey === 'prod_img_cs-brush-96033090') return '/Brushes.png';
     if (storageKey === 'prod_img_pow-1') return '/Cooling Talc.png';
     if (storageKey === 'prod_img_pow-2') return '/Baby Powder.png';
-    if (storageKey === 'prod_img_pow-3') return '/translucent loose setting powder.jpg';
+    if (storageKey === 'prod_img_pow-3') return '/Translucent Loose Setting Powder.jpg';
 return defaultSrc;
 });
   // Load custom image from IndexedDB on mount if available
