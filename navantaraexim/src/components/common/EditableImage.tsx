b825f7d8a90360affcc4098280e1ac46c5c4c0ac
amolgiiri6-star <amol.giiri6@gmail.com>
2026-09-11 02:32:43 +0530

    Fix chewing tobacco preparation image path

--- navantaraexim/src/components/common/EditableImage.tsx
+++ navantaraexim/src/components/common/EditableImage.tsx
@@ -63,7 +63,7 @@ if (storageKey === 'prod_img_eo-3') return '/ginger oil.png';
     if (storageKey === 'prod_img_deo-rollon-sticks-33072000') return '/Botanical_Roll-Ons_Stick.jpg';
     if (storageKey === 'prod_img_deo-creams-solids-33072000') return '/Artisnal deodorants.jpg';
     if (storageKey === 'prod_img_tob-24039910-khaini') return '/Chewing Tobacco - Filter Khaini.png';
-    if (storageKey === 'prod_img_tob-24039920-prep') return '/Preparations Containing Chewing Tobacco (2).png';
+    if (storageKey === 'prod_img_tob-24039920-prep') return '/Preparations Containing Chewing Tobacco.png';
     if (storageKey === 'prod_img_tob-24039930-jarda') return '/zarda_compressed.jpeg';
 return defaultSrc;
 });
